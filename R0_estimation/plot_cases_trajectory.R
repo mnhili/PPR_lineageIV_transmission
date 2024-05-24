aef872fde253ac880eca86412c79571ef30b9c44
manal.nhili <manal.nhili@cirad.fr>
2024-05-24 23:47:26 +0200

    changes applied

--- R0_estimation/plot_cases_trajectory.R
+++ R0_estimation/plot_cases_trajectory.R
@@ -1,35 +1,56 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(plotly)
+
+#install.packages("plotly")
+
 
 # Read your data into a data frame
-data <- read.csv("simulation_results_v2.csv")
+data <- read.csv("simulation_results_v5.csv")
 
 # Reshape data from wide to long format
-long_data <- pivot_longer(data, cols = starts_with("Iteration"), 
+long_data <- pivot_longer(data, cols = starts_with("iter"), 
                           names_to = "simulation", 
                           values_to = "cases")
 
 # Calculate average for each day, size, and density
-avg_data <- long_data %>%
-  group_by(Infectious.Time, Population.Size, Density) %>%
-  summarize(avg_cases = round(mean(cases, na.rm = TRUE)))
+mean_data <- long_data %>%
+  group_by(Infectious_time, Population_size, Density) %>%
+  summarize(avg_cases = mean(cases, na.rm = TRUE)/Infectious_time)
 
-# Create faceted plot
-ggplot(avg_data, aes(x = Infectious.Time, y = avg_cases)) +
-  geom_line() +
-  facet_wrap(~Population.Size + Density, ncol = 3) +
-  labs(x = "Day", y = "Average Number of Cases") +
-  ggtitle("Trajectories of Average Cases by Size and Density")
 
+mean_data_1 <- long_data %>%
+  group_by(Infectious_time, Population_size, Density) %>%
+  summarize(avg_cases = mean(cases, na.rm = TRUE)/Population_size)
+
+# Calculate average for each day, size, and density
+median_data <- long_data %>%
+  group_by(Infectious_time, Population_size, Density) %>%
+  summarize(avg_cases = round(median(cases, na.rm = TRUE)))
 
 # Get unique day values for x-axis ticks
-unique_days <- sort(unique(avg_data$Infectious.Time))
+unique_pop_size <- sort(unique(mean_data$Population_size))
 
-# Create faceted plot with custom x-axis ticks
-ggplot(avg_data, aes(x = Infectious.Time, y = avg_cases)) +
-  geom_line() +
-  facet_wrap(~Population.Size + Density, ncol = 3) +
-  labs(x = "Day", y = "Average Number of Secondary Cases") +
-  ggtitle("Trajectories of Average Secondary Cases by Size and Density") +
-  scale_x_continuous(breaks = unique_days, labels = unique_days)
+
+p <- ggplot(mean_data, aes(x = Population_size, y = avg_cases, color = Density, group = Density)) +
+  geom_line(linewidth = 1) +
+  geom_point()+
+  facet_wrap(~ Infectious_time, ncol = 2) +
+  labs(x = "Population Size", y = "Average number of secondary cases", color = "Density") +
+  ggtitle("Secondary cases Vs Population size by density per Infectious time")+
+  theme_minimal()
+
+
+p
+
+g <- ggplot(mean_data_1, aes(x = Population_size, y = avg_cases, color = Density, group = Density)) +
+  geom_line(linewidth = 1) +
+  geom_point()+
+  facet_wrap(~ Infectious_time, ncol = 2) +
+  labs(x = "Population Size", y = "Average number of secondary cases", color = "Density") +
+  ggtitle("Secondary cases Vs Population size by density per Infectious time")+
+  theme_minimal()
+
+
+g
